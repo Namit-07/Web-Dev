4de4e2838ce1a788831a8ebb5d050339c73e13ff
Namit-07 <namittickoo2006@gmail.com>
2025-08-04 09:26:43 +0530

    Variables, data types & objects

---
+++ Sigma Web Dev course/JavaScript Topics/Video 55/script.js
@@ -0,0 +1,41 @@
+console.log("Hey this is tutorial 55 of Js")
+
+let a = 5;
+// a = a + 1;
+let b = 6;
+let c = "Namit";
+let _a = "Pandey";
+// var 55a = "Pandey"; --> not allowed
+
+// console.log(a+b)
+// console.log(typeof a, typeof b, typeof c)
+{
+    let a = 66;
+    console.log(a)
+}
+console.log(a)
+
+// const a1 = 5;
+// a1 = a1 + 1;
+// Not allowed because a1 is constant
+
+let x = "Namit";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r);
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r);
+
+let o = {
+    "name": "Namit",
+    "job code": 5600,
+    "is_handsome": true 
+}
+console.log(o);
+o.salary = "100 crores";
+console.log(o);
+o.salary = "500 crores"
+console.log(o); 
