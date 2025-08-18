c2e3baeba59ac4cf5b14f8c58eba0cf937f70d88
Namit-07 <namittickoo2006@gmail.com>
2025-08-18 10:33:08 +0530

    CommonJs Vs EcmaScript

---
+++ Sigma Web Dev course/BackEnd topics/Video 86/main.js
@@ -0,0 +1,15 @@
+// import {a, b, d} from "./mymodule.js";
+// console.log(a, b, d);
+
+// import namit from "./mymodule.js";
+// console.log(namit);
+
+// (function(exports,module,require, __filename, __dirname){
+
+         //  Module code actually lives here!!
+         
+// });
+
+const a = require("./mymodule2.js");
+
+console.log(a, __dirname, __filename);
