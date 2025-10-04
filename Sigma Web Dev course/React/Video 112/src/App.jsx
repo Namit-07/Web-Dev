76517f7ee2ecd5b5a4002520f63c8e8d6f9e22f0
Namit-07 <namittickoo2006@gmail.com>
2025-10-04 12:49:36 +0530

    Handling Events

---
+++ Sigma Web Dev course/React/Video 112/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  // const [name, setName] = useState("Namit")
+  const [form, setForm] = useState({email: "", phone: ""})
+
+  const handleClick = () => {
+    alert("Hey I am clicked!!")
+  }
+
+  const handleMouseOver = () => {
+    alert("Hey I am hovered!!")
+  }
+  
+  const handleChange = (e) => {
+    // setName(e.target.value)
+    setForm({...form, [e.target.name]: e.target.value})
+    console.log(form);
+  }
+
+
+
+  return (
+    <>
+      <div className="button">
+        <button onClick={handleClick}>Click Me</button>
+      </div>
+
+      {/* <div className="red" onMouseOver={handleMouseOver}>
+        I am a red div
+      </div>
+       */}
+      <input type="text" name='email' value={form.email} onChange={handleChange} />
+      <input type="text" name='phone' value={form.phone} onChange={handleChange} />
+    </>
+  )
+}
+
+export default App
