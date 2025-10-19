1669f4413ea976767bd1f38049f24b19ace06f65
Namit-07 <namittickoo2006@gmail.com>
2025-10-19 20:33:26 +0530

    style jsx

---
+++ Sigma Web Dev course/Next.js/Video 135/my-app/app/about/page.js
@@ -0,0 +1,30 @@
+"use client"
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+
+    <div className='container'>
+
+      <h1>This is about me</h1>
+      <p>Hey i am a good boy</p>
+
+      <style jsx>
+        {`
+            .container{
+                background-color: red;
+                color: black;
+                }
+                
+                `}
+      </style>
+    </div>
+    <div className="container">
+        Hey i am good
+    </div>
+                </div>
+  )
+}
+
+export default page
