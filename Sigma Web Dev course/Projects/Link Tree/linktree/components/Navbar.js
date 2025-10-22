f0bbbbf867be965e1dd8536955e1e64e29878da6
Namit-07 <namittickoo2006@gmail.com>
2025-10-22 14:03:35 +0530

    in progress

---
+++ Sigma Web Dev course/Projects/Link Tree/linktree/components/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='bg-white w-[90vw] flex justify-between absolute top-12 right-[5vw] rounded-full p-4  '>
+      <div className="logo flex gap-16 items-center px-8">
+        <img className="h-6" src="https://cdn.prod.website-files.com/666255f7f2126f4e8cec6f8f/66634daccb34e6d65a41c76d_download.svg" alt="" />
+
+        <ul className='flex gap-6 text-black items-center'>
+            <li>Products</li>
+            <li>Templates</li>
+            <li>Marketplace</li>
+            <li>Learn</li>
+            <li>Pricing</li>
+        </ul>
+      </div>
+      <div className='flex gap-3'>
+        <button className='login bg-gray-200 px-5 py-4 rounded-md font-bold'>Log in</button>
+        <button className='signup bg-gray-800 text-white rounded-full py-5  px-7 font-bold'>Sign up free</button>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
